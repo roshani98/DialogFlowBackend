e0dccbcda165842315be9e41a611839ae88742cf
Unay Shah <unayshah@gmail.com>
2021-02-26 04:41:35 +0530

    Moved to service

--- src/main/java/com/DB/FruitSalad/Controller/TextController.java
+++ src/main/java/com/DB/FruitSalad/Controller/TextController.java
@@ -1,27 +1,10 @@
 package com.DB.FruitSalad.Controller;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Logger;
 
-import com.DB.FruitSalad.AppConstants;
-import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.dialogflow.cx.v3.DetectIntentRequest;
-import com.google.cloud.dialogflow.cx.v3.DetectIntentResponse;
-import com.google.cloud.dialogflow.cx.v3.Match;
-import com.google.cloud.dialogflow.cx.v3.Match.MatchType;
-import com.google.cloud.dialogflow.cx.v3.MatchIntentRequest;
-import com.google.cloud.dialogflow.cx.v3.MatchIntentResponse;
-import com.google.cloud.dialogflow.cx.v3.QueryInput;
-import com.google.cloud.dialogflow.cx.v3.QueryResult;
-import com.google.cloud.dialogflow.cx.v3.SessionName;
-import com.google.cloud.dialogflow.cx.v3.SessionsClient;
-import com.google.cloud.dialogflow.cx.v3.SessionsSettings;
-import com.google.cloud.dialogflow.cx.v3.TextInput;
+import com.DB.FruitSalad.Service.DialogFlowService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -30,49 +13,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TextController {
-	private static Logger log = Logger.getAnonymousLogger();
 
-	@PostMapping("/textTesting")
-	public ResponseEntity<Map<String, String>> textTest(@RequestParam("navigationPageName") String navigationPageName,
+	@Autowired
+	private DialogFlowService dialogFlowService;
+
+	@PostMapping("/navigationController")
+	public ResponseEntity<Map<String, String>> navigationController(
+			@RequestParam("navigationPageName") String navigationPageName,
 			@RequestParam("sessionId") String sessionId) {
-		Map<String, String> responseMap = new HashMap<>();
-		try {
-			SessionsSettings.Builder sessionsSettingsBuilder = SessionsSettings.newBuilder();
-			sessionsSettingsBuilder.setEndpoint("dialogflow.googleapis.com:443");
-			SessionsSettings sessionsSettings = SessionsSettings.newBuilder()
-					.setCredentialsProvider(
-							FixedCredentialsProvider.create(GoogleCredentials.fromStream(new FileInputStream(
-									new File("E:\\DBTechHackathon Files\\hack-fruitsalad-2805a0bee0a7.json")))))
-					.build();
-			SessionsClient sessionsClient = SessionsClient.create(sessionsSettings);
-			SessionName sessionName = SessionName.of(AppConstants.PROJECT_NAME, AppConstants.LOCATION,
-					AppConstants.NAVIGATION_AGENT, sessionId);
-
-			TextInput.Builder textInput = TextInput.newBuilder().setText(navigationPageName);
-			QueryInput queryInput = QueryInput.newBuilder().setText(textInput).setLanguageCode("en-US").build();
-
-			MatchIntentRequest matchIntent = MatchIntentRequest.newBuilder().setSession(sessionName.toString())
-					.setQueryInput(queryInput).build();
-
-			MatchIntentResponse matchIntentResponse = sessionsClient.matchIntent(matchIntent);
-
-			if (matchIntentResponse.getMatchesCount() == 1
-					&& !matchIntentResponse.getMatches(0).getMatchType().equals(MatchType.NO_MATCH)) {
-				DetectIntentRequest detectIntentRequest = DetectIntentRequest.newBuilder()
-						.setSession(sessionName.toString()).setQueryInput(queryInput).build();
-				DetectIntentResponse detectIntentResponse = sessionsClient.detectIntent(detectIntentRequest);
-				QueryResult queryResult = detectIntentResponse.getQueryResult();
-				System.out.println(queryResult.getResponseMessages(0).getText().getText(0));
-				responseMap.put(AppConstants.PAGE_TYPE, AppConstants.NAVIGATION_KEY);
-				responseMap.put(AppConstants.RESPONSE_TEXT_KEY,
-						queryResult.getResponseMessages(0).getText().getText(0));
-//				System.out.println(queryResult.getResponseMessagesCount());
-//				System.out.println(queryResult.getResponseMessages(queryResult.getResponseMessagesCount()-1));
-			}
-			return new ResponseEntity<>(responseMap, HttpStatus.ACCEPTED);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		return new ResponseEntity<>(dialogFlowService.navigationResponse(navigationPageName, sessionId),
+				HttpStatus.ACCEPTED);
 	}
 }
--- src/main/java/com/DB/FruitSalad/Service/DialogFlowService.java
+++ src/main/java/com/DB/FruitSalad/Service/DialogFlowService.java
@@ -1,10 +1,88 @@
 package com.DB.FruitSalad.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
+import com.DB.FruitSalad.AppConstants;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.dialogflow.cx.v3.DetectIntentRequest;
+import com.google.cloud.dialogflow.cx.v3.DetectIntentResponse;
+import com.google.cloud.dialogflow.cx.v3.MatchIntentRequest;
+import com.google.cloud.dialogflow.cx.v3.MatchIntentResponse;
+import com.google.cloud.dialogflow.cx.v3.QueryInput;
+import com.google.cloud.dialogflow.cx.v3.QueryResult;
+import com.google.cloud.dialogflow.cx.v3.SessionName;
+import com.google.cloud.dialogflow.cx.v3.SessionsClient;
+import com.google.cloud.dialogflow.cx.v3.SessionsSettings;
+import com.google.cloud.dialogflow.cx.v3.TextInput;
+import com.google.cloud.dialogflow.cx.v3.Match.MatchType;
+
 @Service
-public class DialogFlowService{
-    public void dialogFlowFirstTest(){
-        
-    }
+public class DialogFlowService {
+
+	public Map<String, String> navigationResponse(String navigationPageName, String sessionId) {
+		try {
+			SessionsClient sessionsClient = sessionClient(sessionId);
+			SessionName sessionName = SessionName.of(AppConstants.PROJECT_NAME, AppConstants.LOCATION,
+					AppConstants.NAVIGATION_AGENT, sessionId);
+			QueryInput queryInput = getQueryInput(navigationPageName);
+			MatchIntentResponse matchIntentResponse = getMatchIntentResponse(sessionName, sessionsClient, queryInput);
+			QueryResult queryResult = getQueryResponse(navigationPageName, sessionName, sessionsClient, queryInput,
+					matchIntentResponse);
+			return createDataMap(AppConstants.NAVIGATION_KEY, queryResult.getResponseMessages(0).getText().getText(0), null);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public SessionsClient sessionClient(String sessionId) {
+		try {
+			SessionsSettings sessionsSettings = SessionsSettings.newBuilder()
+					.setCredentialsProvider(
+							FixedCredentialsProvider.create(GoogleCredentials.fromStream(new FileInputStream(
+									new File("src\\main\\resources\\hack-fruitsalad-2805a0bee0a7.json")))))
+					.build();
+			return SessionsClient.create(sessionsSettings);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public QueryInput getQueryInput(String text) {
+		TextInput.Builder textInput = TextInput.newBuilder().setText(text);
+		return QueryInput.newBuilder().setText(textInput).setLanguageCode(AppConstants.EN_US).build();
+	}
+
+	public MatchIntentResponse getMatchIntentResponse(SessionName sessionName, SessionsClient sessionsClient,
+			QueryInput queryInput) {
+		MatchIntentRequest matchIntent = MatchIntentRequest.newBuilder().setSession(sessionName.toString())
+				.setQueryInput(queryInput).build();
+		return sessionsClient.matchIntent(matchIntent);
+	}
+
+	public QueryResult getQueryResponse(String text, SessionName sessionName, SessionsClient sessionsClient,
+			QueryInput queryInput, MatchIntentResponse matchIntentResponse) {
+		if (matchIntentResponse.getMatchesCount() == 1
+				&& !matchIntentResponse.getMatches(0).getMatchType().equals(MatchType.NO_MATCH)) {
+			DetectIntentRequest detectIntentRequest = DetectIntentRequest.newBuilder()
+					.setSession(sessionName.toString()).setQueryInput(queryInput).build();
+			DetectIntentResponse detectIntentResponse = sessionsClient.detectIntent(detectIntentRequest);
+			return detectIntentResponse.getQueryResult();
+		}
+		return null;
+	}
+	
+	private Map<String, String> createDataMap(String pageType, String responseText, String capturedValue){
+		Map<String, String> map = new HashMap<>();
+		map.put(AppConstants.PAGE_TYPE, pageType);
+		map.put(AppConstants.RESPONSE_TEXT_KEY, responseText);
+		map.put(AppConstants.CAPTURED_TEXT_KEY, capturedValue);
+		return map;
+	}
 }
