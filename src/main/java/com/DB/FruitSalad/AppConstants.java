6f72c83336e78881138bbc4c4e1b1e6166a3b231
Unay Shah <unayshah@gmail.com>
2021-02-26 01:55:23 +0530

    Completed navigation agent. Added app constants.

---
+++ src/main/java/com/DB/FruitSalad/AppConstants.java
@@ -0,0 +1,12 @@
+package com.DB.FruitSalad;
+
+public interface AppConstants {
+	final public static String PAGE_TYPE = "page_type";
+	final public static String NAVIGATION_KEY = "navigation";
+	final public static String FORM_KEY = "form";
+	final public static String RESPONSE_TEXT_KEY = "response";
+	final public static String PROJECT_NAME = "hack-fruitsalad";
+	final public static String LOCATION = "hack-fruitsalad";
+	final public static String NAVIGATION_AGENT = "3d1f2c36-feea-4c2e-9fd8-4f217d0b2565";
+	final public static String FORM_AGENT = "3ebfe9c9-10de-4369-bb95-16e1005e8f9a";
+}
